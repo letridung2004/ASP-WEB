2c92d6395ae58bb97152205b71b1519d34c5178b
letridung2004 <tridung301004@gmail.com>
2024-09-07 08:41:26 +0700

    hoan thanh bai tap

---
+++ BaiTapVeNha02/Controllers/Tuan2Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha02.Controllers
+{
+    public class Tuan2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Le Tri Dung";
+            ViewBag.MSSV = "1822041040";
+            ViewBag.Nam = 2024;
+
+            return View();
+        }
+    }
+}

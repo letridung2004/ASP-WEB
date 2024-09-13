053b84a4fb57950fd92e3aaeb20047effe8b249a
letridung2004 <tridung301004@gmail.com>
2024-09-14 00:57:31 +0700

    hoan thanh bai tap

---
+++ BaiThucHanh06/BaiThucHanh03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaiThucHanh03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ BaiThucHanh06/BaiThucHanh03/Models/TaiKhoanModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BaiThucHanh03.Models
+{
+    public class TaiKhoanModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}

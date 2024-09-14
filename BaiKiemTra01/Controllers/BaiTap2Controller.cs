f41c8334bb5750f8491772dc901c616ef0612ee7
letridung2004 <tridung301004@gmail.com>
2024-09-14 15:17:24 +0700

    hoan thanh bai tap

---
+++ BaiKiemTra01/Controllers/BaiTap2Controller.cs
@@ -0,0 +1,23 @@
+﻿using BaiKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra01.Controllers
+{
+    public class BaiTap2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BaiTap2(SanPhamViewModel SanPham)
+        {
+            var sanpham = new SanPhamViewModel()
+            {
+                TenSP = "LapTop",
+                GiaBan = 15000000,
+                AnhMoTa = "/wwwroot/images/laptop.jpg"
+            };
+            return View(sanpham);
+        }
+    }
+}

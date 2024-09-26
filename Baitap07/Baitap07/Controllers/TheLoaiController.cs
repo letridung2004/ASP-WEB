31007ef835944ae99ae468851328c2ccf8bc513f
letridung2004 <tridung301004@gmail.com>
2024-09-26 12:14:03 +0700

    hoan thanh bai tap

---
+++ Baitap07/Baitap07/Controllers/TheLoaiController.cs
@@ -0,0 +1,34 @@
+﻿using Baitap07.Data;
+using Baitap07.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap07.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TheLoaiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var theloai = _db.TheLoai.ToList();
+            ViewBag.TheLoai = theloai;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TheLoai theloai)
+        {
+            _db.TheLoai.Add(theloai);
+            _db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Baitap07/Baitap07/Models/TheLoai.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Baitap07.Models
+{
+    public class TheLoai
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime Description { get; set; } = DateTime.Now;
+    }
+}

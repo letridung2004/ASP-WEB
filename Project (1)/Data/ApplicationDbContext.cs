df42656e0de591fe9f55a4bd3d04ac474e081129
letridung2004 <tridung301004@gmail.com>
2024-10-18 08:58:12 +0700

    hoan thanh bai tap

---
+++ Project (1)/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project1.Models;
+
+namespace Project1.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<TheLoai> TheLoai { get; set; }
+		public DbSet<SanPham> SanPham { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ Project (1)/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        public string? Address { get; set; }
+    }
+}
